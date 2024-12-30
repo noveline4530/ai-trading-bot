4738faf427fa4a92beb6743ec518744cdc130053
SeokJun Kim <sjun4530@gmail.com>
2024-12-31 02:42:11 +0900

    hotfix

--- autotrade_sj_v1.py
+++ autotrade_sj_v1.py
@@ -10,14 +10,10 @@ from openai import OpenAI
 import schedule
 import time
 import requests
+from PIL import Image
+import io
 from datetime import datetime
 import sqlite3
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
